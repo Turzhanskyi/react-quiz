c69423756031cbfdb72110ab9c969ca7eaabce04
Turzhanskyi <turzhansky81@gmail.com>
2020-03-08 22:08:36 +0200

    43. Создание Layout

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
-import React from "react";
+import React, { Component } from "react";
+import Layout from "./hoc/Layout/Layout";
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Hello react quiz</h1>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <Layout>
+        <div style={{ width: 400, border: "1px solid black" }}>
+          <h1>Layout works</h1>
+        </div>
+      </Layout>
+    );
+  }
 }
-
 export default App;
