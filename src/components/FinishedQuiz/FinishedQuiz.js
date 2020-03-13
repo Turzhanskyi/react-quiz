23ca09682c8a7e73cdeba3b0dd29c1f705f832b1
Turzhanskyi <turzhansky81@gmail.com>
2020-03-14 00:51:16 +0200

    73. Ссылка через кастомный компонент

--- src/components/FinishedQuiz/FinishedQuiz.js
+++ src/components/FinishedQuiz/FinishedQuiz.js
@@ -1,6 +1,7 @@
 import React from "react";
 import classes from "./FinishedQuiz.module.css";
 import Button from "../UI/Button/Button";
+import { Link } from "react-router-dom";
 
 const FinishedQuiz = props => {
   const successCount = Object.keys(props.results).reduce((total, key) => {
@@ -38,7 +39,9 @@ const FinishedQuiz = props => {
         <Button onClick={props.onRetry} type="primary">
           Повторити
         </Button>
-        <Button type="success">Перейти до списку тестів</Button>
+        <Link to="/">
+          <Button type="success">Перейти до списку тестів</Button>
+        </Link>
       </div>
     </div>
   );
