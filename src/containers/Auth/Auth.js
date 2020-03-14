78dcfd3e328e8a00bd2b19ba8062a7903aae7998
Turzhanskyi <turzhansky81@gmail.com>
2020-03-14 12:53:02 +0200

    74. Страница авторизации

--- src/containers/Auth/Auth.js
+++ src/containers/Auth/Auth.js
@@ -1,10 +1,34 @@
 import React, { Component } from "react";
-
+import classes from "./Auth.module.css";
+import Button from "../../components/UI/Button/Button";
 export default class Auth extends Component {
+  loginHandler = () => {};
+
+  registerHandler = () => {};
+
+  submitHandler = event => {
+    event.preventDefault();
+  };
+
   render() {
     return (
-      <div>
-        <h1>Auth</h1>
+      <div className={classes.Auth}>
+        <div>
+          <h1>Авторизація</h1>
+
+          <form onSubmit={this.submitHandler} className={classes.AuthForm}>
+            <input type="text" />
+            <input type="text" />
+
+            <Button type="success" onClick={this.loginHandler}>
+              Увійти
+            </Button>
+
+            <Button type="primary" onClick={this.registerHandler}>
+              Зареєструватися
+            </Button>
+          </form>
+        </div>
       </div>
     );
   }
