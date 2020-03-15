a448b95ca49696a2b9338ea5db1ebf61af591295
Turzhanskyi <turzhansky81@gmail.com>
2020-03-16 01:27:40 +0200

    86. Загрузка списка тестов

--- src/containers/QuizList/QuizList.js
+++ src/containers/QuizList/QuizList.js
@@ -4,29 +4,48 @@ import { NavLink } from "react-router-dom";
 import axios from "axios";
 
 export default class QuizList extends Component {
+  state = {
+    quizes: []
+  };
+
   renderQuizes() {
-    return [1, 2, 3].map((quiz, index) => {
+    return this.state.quizes.map(quiz => {
       return (
-        <li key={index}>
-          <NavLink to={"/quiz/" + quiz}>Тест {quiz}</NavLink>
+        <li key={quiz.id}>
+          <NavLink to={"/quiz/" + quiz.id}>{quiz.name}</NavLink>
         </li>
       );
     });
   }
 
-  componentDidMount() {
-    axios
-      .get("https://react-quiz-dc3aa.firebaseio.com/quiz.json")
-      .then(response => {
-        console.log(response);
+  async componentDidMount() {
+    try {
+      const response = await axios.get(
+        "https://react-quiz-dc3aa.firebaseio.com/quizes.json"
+      );
+
+      const quizes = [];
+
+      Object.keys(response.data).forEach((key, index) => {
+        quizes.push({
+          id: key,
+          name: `Тест №${index + 1}`
+        });
       });
+
+      this.setState({
+        quizes
+      });
+    } catch (e) {
+      console.log(e);
+    }
   }
 
   render() {
     return (
       <div className={classes.QuizList}>
         <div>
-          <h1>Список тестов</h1>
+          <h1>Список тестів</h1>
 
           <ul>{this.renderQuizes()}</ul>
         </div>
