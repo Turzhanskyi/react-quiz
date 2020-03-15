3b96766cc122f8220d2b001a2199e0c75f47e555
Turzhanskyi <turzhansky81@gmail.com>
2020-03-16 01:03:57 +0200

    85. Создание теста

--- src/containers/QuizCreator/QuizCreator.js
+++ src/containers/QuizCreator/QuizCreator.js
@@ -9,6 +9,7 @@ import {
   validateForm
 } from "../../form/formFramework";
 import Auxiliary from "../../hoc/Auxiliary/Auxiliary";
+import axios from "axios";
 
 function createOptionControl(number) {
   return createControl(
@@ -85,10 +86,24 @@ export default class QuizCreator extends Component {
     });
   };
 
-  createQuizHandler = event => {
+  createQuizHandler = async event => {
     event.preventDefault();
 
-    console.log(this.state.quiz);
+    try {
+      await axios.post(
+        "https://react-quiz-dc3aa.firebaseio.com/quizes.json",
+        this.state.quiz
+      );
+
+      this.setState({
+        quiz: [],
+        rightAnswerId: 1,
+        isFormValid: false,
+        formControls: createFormControls()
+      });
+    } catch (e) {
+      console.log(e);
+    }
   };
 
   changeHandler = (value, controlName) => {
